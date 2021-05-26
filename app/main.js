a9cd94201e27a3600d1c55ccf4a0d0a41e8c2f32
fxjavi <fjmarcoschavez@gmail.com>
2021-05-26 10:30:47 +0200

    Added files

---
+++ app/main.js
@@ -0,0 +1,120 @@
+import '../assets/sass/main.scss'
+
+
+window.addEventListener("load", () => {
+    loadMapView();
+    renderMap();
+    renderMarkers();
+    initMapEvents();
+});
+
+let markersPositions;
+let mapPosition; 
+let view;
+let map;
+let markers;
+
+const loadMarkers = () => {
+
+};
+
+const loadMapInfo = () => {
+
+};
+
+const loadMapView = () => {
+    loadMarkers();
+    loadMapInfo();
+
+    renderMapViewHeader();
+    renderMapViewMain();
+    renderMapViewFooter();
+};
+
+const renderMapViewHeader = () => {
+
+};
+
+const renderMapViewMain = () => {
+
+};
+
+const renderMapViewFooter = () => {
+    
+    flyToLocation();
+};
+
+const renderMap = () => {
+
+};
+
+const renderMarkers = () => {
+
+};
+
+const flyToLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lng = position.coords.longitude;
+        const lat = position.coords.latitude;
+
+        map.flyTo({
+            center: [lng, lat],
+            zoom: 9
+        })
+    });
+};
+
+const initMapEvents = () => {
+    map.on("move", (ev) => {
+        const center = ev.target.getCenter();
+        const zoom = ev.target.getZoom();
+        const storingObj = {
+            lat: center.lat,
+            lng: center.lng,
+            zoom: zoom
+        };
+        localStorage.setItem("center", JSON.stringify(storingObj));
+    });
+
+    map.on("click", async(ev) => {
+        loadSingleView(ev.lngLat);
+    });
+};
+
+const loadSingleView = (lngLat) => {
+    loadSpinner();
+    fetchData();
+
+    renderSingleViewHeader();
+    renderSingleViewMain();
+    renderSingleViewFooter();
+};
+
+const loadSpinner = () => {
+
+};
+
+const unloadSpinner = () => {
+
+};
+
+const fetchData = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ev.lngLat.lat}&lon=${ev.lngLat.lng}&appid=b9`;
+    unloadSpinner();
+};
+
+const renderSingleViewHeader = () => {
+
+};
+
+const renderSingleViewMain = () => {
+
+};
+
+const renderSingleViewFooter = () => {
+
+};
+
+const saveMarker = () => {
+    loadMapView();
+};
